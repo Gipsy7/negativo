2fb15081392eef0c03a0e8ee5db6d7fd15dbbf25
Mikael Francisco <mikael.francisco@w12.com.br>
2025-09-24 22:48:41 -0300

    feat: connect TransactionForm to TransactionUser for adding transactions

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -5,6 +5,10 @@ class TransactionForm extends StatelessWidget {
   final valueController = TextEditingController();
   final _baseColor = const Color.fromARGB(255, 216, 19, 95);
 
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -26,8 +30,9 @@ class TransactionForm extends StatelessWidget {
                     ),
                   ),
                   TextButton(onPressed:   (){
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text.isEmpty ? 'Gasto aleatório' : titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, value);
                   },
                     child: Text('Adicionar transação', style: TextStyle(color: _baseColor),
                     ),
--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -36,8 +36,8 @@ class _TransactionUserState extends State<TransactionUser> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        TransactionList(  _transactions),
-        TransactionForm()
+        TransactionList(_transactions),
+        TransactionForm(_addTransaction)
       ],
     );
   }
